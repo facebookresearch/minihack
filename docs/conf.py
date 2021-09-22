43ac422c03ad94264c62168705a090046981cde4
samvelyan <mika.samvelyan@gmail.com>
2021-09-22 10:38:42 +0100

    Making the sphinx api-doc compile on the RTD servers.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ import sys
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
 
+autodoc_mock_imports = ["nle"]
 
 # -- Project information -----------------------------------------------------
 
