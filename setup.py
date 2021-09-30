0e6133926c7e9eed53cfecfe2bd51dfefc1e25a4
Mikayel Samvelyan <mika.samvelyan@gmail.com>
2021-09-30 18:05:19 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         long_description = f.read()
     cwd = os.path.dirname(os.path.abspath(__file__))
     sha = "Unknown"
-    version = "0.1.0"
+    version = "0.1.1"
 
     try:
         sha = (
