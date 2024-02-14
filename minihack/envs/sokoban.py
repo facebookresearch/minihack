a13e109d9f18ee0ea4b3ec485bfc04c0500c8aaf
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-02-14 15:57:55 +0100

    add auxiliary reward for pushing boulders into pits

--- minihack/envs/sokoban.py
+++ minihack/envs/sokoban.py
@@ -1,4 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+import numpy as np
+
 from minihack.envs import register
 from minihack import MiniHackNavigation
 from nle import nethack
@@ -15,46 +17,99 @@ class Sokoban(MiniHackNavigation):
     def __init__(self, *args, **kwargs):
         kwargs["max_episode_steps"] = kwargs.pop("max_episode_steps", 2000)
         kwargs["actions"] = kwargs.pop("actions", NAVIGATE_ACTIONS)
+
+        self._time_penalty = kwargs.pop("penalty_time", 0)
+        self._reward_shaping_coefficient = kwargs.pop(
+            "reward_shaping_coefficient", 0
+        )
+
         super().__init__(*args, **kwargs)
+
+    def _is_episode_end(self, observation):
+        result = super()._is_episode_end(observation)
+
+        if result == self.StepStatus.TASK_SUCCESSFUL:
+            # all pits should be filled
+            if len(self._object_positions(self.last_observation, "^")) != 0:
+                return self.StepStatus.RUNNING
+
+        return result
+
+    def _reward_fn(self, last_observation, action, observation, end_status):
+        if end_status == self.StepStatus.TASK_SUCCESSFUL:
+            return 1
+        elif end_status != self.StepStatus.RUNNING:
+            return 0
+        # add additional reward if pit is filled (number of pits is decreased)
+        return (
+            self._time_penalty
+            + (
+                len(self._object_positions(last_observation, "^"))
+                - len(self._object_positions(observation, "^"))
+            )
+            * self._reward_shaping_coefficient
+        )
+
+    def _object_positions(self, observation, object_char):
+        char_obs = observation[self._original_observation_keys.index("chars")]
+        return set(
+            (x, y) for x, y in zip(*np.where(char_obs == ord(object_char)))
+        )
 
 
 class MiniHackSokoban1a(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko1a.des", **kwargs)
 
 
 class MiniHackSokoban1b(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko1b.des", **kwargs)
 
 
 class MiniHackSokoban2a(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko2a.des", **kwargs)
 
 
 class MiniHackSokoban2b(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko2b.des", **kwargs)
 
 
 class MiniHackSokoban3a(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko3a.des", **kwargs)
 
 
 class MiniHackSokoban3b(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko3b.des", **kwargs)
 
 
 class MiniHackSokoban4a(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko4a.des", **kwargs)
 
 
 class MiniHackSokoban4b(Sokoban):
     def __init__(self, *args, **kwargs):
+        kwargs["reward_shaping_coefficient"] = 0.1
+        kwargs["penalty_time"] = -0.001
         super().__init__(*args, des_file="soko4b.des", **kwargs)
 
 
